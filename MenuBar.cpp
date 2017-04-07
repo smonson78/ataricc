381bfb3ab8c51380baada34d31a72ed3e09b18b7
Simon <simon@smonson.com>
2017-04-07 23:24:18 +1000

    Cleaned up the menu bar generator

--- MenuBar.cpp
+++ MenuBar.cpp
@@ -13,6 +13,7 @@ MenuBar::MenuBar() {
 MenuBar::~MenuBar() {
 }
 
+/* This function is massive. It would be good to make it a compile-time option. */
 OBJECT *MenuBar::buildObjectArray() {
 	OBJECT *p = new OBJECT[30];
 
@@ -29,45 +30,48 @@ OBJECT *MenuBar::buildObjectArray() {
 	p[1].ob_head = 2;
 	p[1].ob_tail = 2;
 
-	// THE ACTIVE
-	// FIXME: width should be total width of all menus
-	new_object(&p[2], OBJ_IBOX, (void *)0, 2 * 8, 0, 30 * 8, 16 + 3);
-	p[2].ob_next = 1;
-	p[2].ob_head = 3;
-	p[2].ob_tail = -1;
+	// Do the ACTIVE (p[2]) after this bit so we'll know how wide it is
 
 	// Add menu titles
 	int o = 3, i = 0;
-	int menu_start_x = 2 * 8;
+	int menu_start_x = 0;
+	int total_menu_width = 0;
 	smonson::LinkedListNode<Menu> *n;
 	for (n = contents.getHead(); n; n = n->getNext()) {
 		Menu *item = n->getItem();
 		int width = strlen(item->getTitle()) * 8;
+		total_menu_width += width;
 		menu_x[i] = menu_start_x;
 		o = 3 + i;
 		new_object(&p[o], OBJ_GTITLE, (void *)item->getTitle(),
-				// give it 15 chars width for now
 				menu_start_x, 0, width, 16 + 3);
 		menu_start_x += width;
 		p[o].ob_next = o + 1;
 		p[o].ob_head = -1;
 		p[o].ob_tail = -1;
-		p[2].ob_tail = o; // update pointer from ACTIVE
 
 		i++;
 	}
-	// update last pointer to point back to ACTIVE
+
+	// fill in THE ACTIVE
+	new_object(&p[2], OBJ_IBOX, (void *)0, 2 * 8, 0, total_menu_width, 16 + 3);
+	p[2].ob_next = 1;
+	p[2].ob_head = 3;
+	p[2].ob_tail = o; // update pointer from ACTIVE to the last menu title
+
+	// update last menu title's 'next' pointer to point back to ACTIVE
 	p[o].ob_next = 2;
 	o++;
 
-	// THE SCREEN (5)
-	// FIXME: shrink to fit menus
+	// THE SCREEN
 	int i_screen = o;
+	int screen_width = 0;
+	int screen_height = 0;
 
 	new_object(&p[i_screen], OBJ_IBOX, (void *)0, 0, 16 + 3, 80 * 8, 8 * 16);
 	p[i_screen].ob_next = 0;
 	int i_menu = o + 1;
-	p[i_screen].ob_head = i_menu; // FIXME: if present. pointer to menus
+	p[i_screen].ob_head = i_menu; // FIXME: if menus are present. pointer to menus
 
 	p[0].ob_tail = o; // update ROOT pointer
 	p[1].ob_next = o; // update BAR pointer
@@ -80,11 +84,13 @@ OBJECT *MenuBar::buildObjectArray() {
 
 		o++;
 		i_menu = o;
+
+		// The box around the drop-down part of the menu
 		new_object(&p[i_menu], OBJ_GBOX, (void *)0xff1100,
 				menu_x[i] + (2 * 8), 0, 0, 0);
 		p[i_menu].ob_head = i_menu + 1;
 		p[i_menu].ob_tail = -1;
-		p[i_screen].ob_tail = o; // update pointer from ACTIVE
+		p[i_screen].ob_tail = o; // update pointer from SCREEN
 
 		// Add menu items
 		smonson::LinkedListNode<MenuItem> *s;
@@ -100,7 +106,7 @@ OBJECT *MenuBar::buildObjectArray() {
 			p[o].ob_next = o + 1;
 			p[o].ob_head = -1;
 			p[o].ob_tail = -1;
-			p[i_screen].ob_tail = o; // update pointer from ACTIVE
+			p[i_screen].ob_tail = o; // update pointer from SCREEN
 
 			if (max_width < width)
 				max_width = width;
@@ -120,16 +126,23 @@ OBJECT *MenuBar::buildObjectArray() {
 		p[i_menu].ob_next = o + 1; // pointer to next menu
 		p[i_menu].ob_tail = o; // tail of menu's list
 		p[o].ob_next = i_menu; // last pointer back to menu
+
+		// keep track of max width/height
+		screen_width = (p[i_menu].ob_x + p[i_menu].ob_width) > screen_width ?
+				(p[i_menu].ob_x + p[i_menu].ob_width) : screen_width;
+		screen_height = p[i_menu].ob_width > screen_height ?
+				p[i_menu].ob_width : screen_height;
 		i++;
 	}
 	p[i_screen].ob_tail = i_menu; // tail of SCREEN's list
 	p[i_menu].ob_next = i_screen; // last pointer back to SCREEN
 
+	// max size of SCREEN
+	p[i_screen].ob_width = screen_width;
+	p[i_screen].ob_height = screen_height;
+
 	o++;
-	//printf("Menu size: %d\n", o);
 
-
-//	menu_bar(p, MENU_SHOW);
 	return p;
 }
 
